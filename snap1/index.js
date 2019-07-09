312804bd5eb78cd4e5eea42c84607551db034309
CodeCanna <fleabass4@gmail.com>
2019-07-09 08:43:22 -0600

    Assignment Done

---
+++ snap1/index.js
@@ -0,0 +1,10 @@
+function add(num1, num2) {
+	return num1 + num2
+}
+
+function contc(string1, string2) {
+	return string1 + string2;
+}
+
+console.log(add(4, 4));
+console.log(add("Hello ", "World!"));
