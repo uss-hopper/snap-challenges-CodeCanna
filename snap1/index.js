cc0c58c9d50170ecbec987eb11a45d8679946c2b
CodeCanna <fleabass4@gmail.com>
2019-07-11 08:35:22 -0600

    Snap Challenge 2 Completed

--- snap1/index.js
+++ snap1/index.js
@@ -1,3 +1,4 @@
+/*
 function add(num1, num2) {
 	return num1 + num2
 }
@@ -8,3 +9,19 @@ function contc(string1, string2) {
 
 console.log(add(4, 4));
 console.log(add("Hello ", "World!"));
+*/
+
+var elevatorPitch = {
+	Name: "My name is Mark.",
+	What: "I make spatulas that make the perfect omelet no matter you skill level.",
+	Why: "I made this product to help everyone enjoy the perfect omelet.",
+	How: "Pattent pending epic non-stick technology",
+	Question: "Why not make the perfect omelet?",
+};
+
+function pitch(name, what, why, how, question) {
+	var thePitch = name + " " + what + " " + why + "\n" + how + " " + question;
+	console.log(thePitch);
+}
+
+pitch(elevatorPitch.Name, elevatorPitch.What, elevatorPitch.Why, elevatorPitch.How, elevatorPitch.Question);
